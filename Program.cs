2827d8cb5334a7a05a7df599087bcb57e7f46781
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-12 12:47:37 +0530

    Welcome to Test Maximum program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 50; int c = 30;
+
+
+            Console.WriteLine("Max number is " + MaxNumber<int>.printMaximumNumbers(a, b, c));
+
+        }
+    }
+}
