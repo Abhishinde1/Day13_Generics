d8d517cf99d43651bd8874294966553764d07bd8
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-12 12:54:33 +0530

    Max Float Program Added

---
+++ MaxFloat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+ 
+        internal class MaxFloat<T> where T : IComparable
+        {
+            public static T printMaximumNumbers(T num1, T num2, T num3)
+            {
+
+                if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0
+                    || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0
+                        || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+                {
+                    return num1;
+
+                }
+                if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0
+                    || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0
+                        || num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+                {
+                    return num2;
+
+                }
+                if (num3.CompareTo(num2) > 0 && num3.CompareTo(num1) > 0
+                    || num3.CompareTo(num2) >= 0 && num3.CompareTo(num1) > 0
+                        || num3.CompareTo(num2) > 0 && num3.CompareTo(num1) >= 0)
+                {
+                    return num3;
+
+                }
+                return default;
+
+            }
+        }
+}
